a2686a986dd975d162bb59b588620f59902bf72b
Wiktoria van Harneveldt <62669899+Braweria@users.noreply.github.com>
2023-09-07 21:32:37 +0200

    chore: configure project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        outer: "0 0 0 0.5rem var(--tw-shadow-color)",
+      },
+    },
+    plugins: [],
+  },
+};
