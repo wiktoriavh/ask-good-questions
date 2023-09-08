14a50089cb782258abaacf6442a595d505c17347
Wiktoria van Harneveldt <62669899+Braweria@users.noreply.github.com>
2023-09-08 19:25:33 +0200

    feat: create data state

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 import Alpine from "alpinejs";
+import marked from "marked";
 
 window.Alpine = Alpine;
+
+Alpine.data("question", () => ({
+  summarise: "",
+  problem: "",
+  solving: "",
+  expectation: "",
+  markup: "<strong>Nothing to preview yet</strong>",
+
+  init() {
+    console.log("init");
+    this.$watch("summarise", () => this.output());
+    this.$watch("problem", () => this.output());
+    this.$watch("solving", () => this.output());
+    this.$watch("expectation", () => this.output());
+  },
+
+  output() {
+    console.log("output");
+    const combined = `${this.summarise}
+
+    # The Problem
+    ${this.problem}
+    
+    # What I tried
+    ${this.solving}
+    
+    # What I expect to happen
+    ${this.expectation}`;
+
+    this.markup = marked.parse(combined);
+  },
+}));
 
 Alpine.start();
