d63015579798139bde8ed98394e7ef3bc90b2957
Wiktoria van Harneveldt <62669899+wiktoriavh@users.noreply.github.com>
2023-10-17 21:08:05 +0200

    chore: write configs for eslint and prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  bracketSameLine: true,
+  // importOrder: ['@/*$', '~/*$', '^[./]'],
+  // importOrderSeparation: true,
+  // importOrderSortSpecifiers: true,
+  tabWidth: 2,
+};
